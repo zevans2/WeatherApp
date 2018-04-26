656f0a43c9dfed6c4c8776adea2ca9d752a8b849
Zach <zevans2@missouriwestern.edu>
2018-04-26 02:14:38 -0500

    Began implementing GUI via Scenebuilder. Currently does not compile, breaking at line 12 where the Parent root is being initialized.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("views/sample.fxml"));
+        primaryStage.setTitle("Weather");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
@@ -18,7 +18,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         //Create Weather station that holds functions needed to interface with GUI
-
         System.out.println("Connecting to Weather Station");
         WeatherStation station = new WeatherStation();
         System.out.println("Connected: "+station.status);
